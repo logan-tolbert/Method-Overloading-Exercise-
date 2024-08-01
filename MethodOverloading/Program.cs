8be9aec75b39e8c2bd45286cf7b49dd7a9e5012e
Logan <158816171+logan-tolbert@users.noreply.github.com>
2024-08-01 17:25:41 -0500

    Fix: method name casing

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -13,16 +13,16 @@ namespace MethodOverloading
             int num2 = 8;
             decimal decNum1 = 3.14m;
             decimal decNum2 = 5.3434m;
-            int sum = add(num1, num2);
-            decimal decSum = add(decNum1, decNum2);
+            int sum = Add(num1, num2);
+            decimal decSum = Add(decNum1, decNum2);
             bool enableMessage = true;
             
             // messages
             string message = $"{num1} + {num2} = {sum}\n";
             string message2 = $"{decNum1} + {decNum2} = {decSum}\n";
-            string message3 = add(num1 = 1, num2 = 0, enableMessage);
-            string message4 = add(num1 = 6, num2 = 8, enableMessage);
-            string message5 = add(num1, num2, enableMessage = false);
+            string message3 = Add(num1 = 1, num2 = 0, enableMessage);
+            string message4 = Add(num1 = 6, num2 = 8, enableMessage);
+            string message5 = Add(num1, num2, enableMessage = false);
 
             // ouput
             Console.WriteLine(message);
@@ -35,19 +35,19 @@ namespace MethodOverloading
         }
 
         // method definitions
-        public static int add(int x, int y)
+        public static int Add(int x, int y)
         {
             return x + y;
         }
             
         
-        public static decimal add(decimal x, decimal y) 
+        public static decimal Add(decimal x, decimal y) 
         { 
             return (decimal)x + y;
         }
 
 
-        public static string add(int x, int y, bool enableMessage)
+        public static string Add(int x, int y, bool enableMessage)
         {
             int sum = x + y;
             string message1 = $"{sum} dollar\n";
